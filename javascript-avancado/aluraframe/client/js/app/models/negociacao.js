db32c5a8aef28d5707f69c623b6a08275383cdf6
jessikacostac <jessika.cost@gmail.com>
2018-10-04 11:55:06 -0300

    Programação Defensiva

--- javascript-avancado/aluraframe/client/js/app/models/negociacao.js
+++ javascript-avancado/aluraframe/client/js/app/models/negociacao.js
@@ -2,19 +2,25 @@ class Negociacao {
 
     constructor(data, quantidade, valor) {
 
-        this._data = data;
+        //Por padrão quando existe o "_" no começo, quer dizer que este metodo não pode ser alterado.
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+
+        //Não congela a propriedade _data por se tratar de um objeto
+        //Superfície rasa de segurança
+        Object.freeze(this);
     }
 
     //Métodos acessadores -  propriedade GETTER acesso restrito
-   
     get volume() {
         return this._quantidade * this._valor;
     }
 
     get data() {
-        return this._data;
+
+        //Progrmação DEFENSIVA, cria um novo objeto que cria uma cópia da data como uma camada de proteção - IMUTABILIDADE - solução viável e aceita
+        return new Date(this._data.getTime());
     }
 
     get quantidade() {
